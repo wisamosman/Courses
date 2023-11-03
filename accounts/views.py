eb1406eeb0557da1a403abe492053bd90fa7f697
wisamosman <viesam_1987@hotmail.com>
2023-11-03 21:37:20 +0100

    ADD forms for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile,Phones
+from .forms import SignupForm,ActivateUser
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = SignupForm()
+
+    return render(request,'registration/signup.html',{'form':form})            
 
 # Create your views here.
